e8829543e20a6e35b3992696907a59e588b01318
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-06-01 20:29:48 -0700

    building out form

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+    state = {
+      toDo: ["take out the garbage",
+       "do the laundry",
+       "finish homework"],
+      
+    }
+    handleChange = (e)=> {
+      this.setState({ 
+        toDo: e.target.value 
+      })
+    }
+    handleSubmit = (e) => {
+      e.preventDefault();
+    }
   render() {
-    return (
-      <div>
+    return (<div className = "App">
         <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
+      <form onSubmit = {this.handleSubmit}>
+      <input onChange={this.handleChange}/>
+      <button> Enter</button>    
+      <button> Clear To Do List </button>
+      </form>
+      <h2>{this.state.toDo}</h2>
+   </div>);
   }
 }
-
 export default App;
+
